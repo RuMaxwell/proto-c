6a03bcdb39bd0edaa1dc0ae2b5e1a55845d0d1ba
RuMaxwell <935906960@qq.com>
2020-09-13 01:02:04 +0800

    Update Makefile.py to make better experience

--- Makefile.py
+++ Makefile.py
@@ -1,16 +1,35 @@
+'''
+python Makefile.py Foo --> Build with test/FooTest.c and run
+'''
+
 import os
 import sys
+
+pathjoin = os.path.join
 
 projroot = '.'
 with open('.projroot', 'r') as f:
   lines = f.readlines()
-  projroot = lines[0]
+  reg = {}
+  for line in lines:
+    line = line.strip().split(' ', 1)
+    reg[line[0].strip()] = line[1].strip()
+  if sys.platform.startswith('win'):
+    projroot = reg['Windows:']
+  elif sys.platform.startswith('linux'):
+    projroot = reg['Linux:']
 
 def srcfilter(entry: str):
   return entry.endswith('.c')
 
 def srcmapper(entry: str):
-  return projroot + '/src/' + entry
+  return pathjoin(projroot, 'src', entry)
+
+def objectfilter(entry: str):
+  return entry.endswith('.o')
+
+def objectmapper(entry: str):
+  return pathjoin(projroot, entry)
 
 def join(xs: list):
   s = ''
@@ -18,14 +37,39 @@ def join(xs: list):
     s += x + ' '
   return s
 
-srcfiles = join(list(map(srcmapper, filter(srcfilter, os.listdir(projroot + '/src')))))
+srcfiles = join(list(map(srcmapper, filter(srcfilter, os.listdir(pathjoin(projroot, 'src'))))))
 
-test = 'Main'
+test = 'clean'
 if len(sys.argv) > 1:
   test = sys.argv[1]
 
-def testmapper(name: str):
-  return projroot + '/test/' + test + 'Test.c'
+if test == 'clean':
+  objfiles = list(map(objectmapper, filter(objectfilter, os.listdir(projroot))))
+  for entry in objfiles:
+    print(entry)
+    os.remove(entry)
+  os.remove(pathjoin(projroot, 'a'))
+  exit(0)
 
-os.system('gcc -g -c ' + srcfiles + testmapper(test))
-os.system('gcc -o a *.o')
+def testmapper(name: str) -> str:
+  testpath = pathjoin(projroot, 'test')
+  ls = os.listdir(testpath)
+  for entry in ls:
+    if entry == name + 'Test.c':
+      return pathjoin(testpath, name + 'Test.c')
+  raise IOError('Test module %s not found' % name)
+
+testfile = testmapper(test)
+
+CC = 'gcc -g -c ' + srcfiles + testmapper(test)
+print(CC)
+os.system(CC)
+
+objfiles = join(list(map(objectmapper, filter(objectfilter, os.listdir(projroot)))))
+
+LD = 'gcc -o a ' + objfiles
+print(LD)
+os.system(LD)
+
+print('\nTest starting...\n----------------')
+os.system(pathjoin(projroot, 'a'))
